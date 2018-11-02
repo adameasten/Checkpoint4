11b5dfc2837cb7e93afbb6f77f915701ad8ee541
Adam <adaost0110@hotmail.com>
2018-11-02 15:25:40 +0100

    Added another action movie

--- Checkpoint4.0/Program.cs
+++ Checkpoint4.0/Program.cs
@@ -8,6 +8,7 @@ namespace Checkpoint4._0
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Best Movies Ever!");
+            Console.WriteLine("Random action movie  name: War for Action");
         }
     }
 }

bf017ad313e24c82d227ba33f2ff2deb25949f9c
Adam <adaost0110@hotmail.com>
2018-11-02 15:10:25 +0100

    Added copyright statement

--- Checkpoint4.0/Program.cs
+++ Checkpoint4.0/Program.cs
@@ -8,6 +8,7 @@ namespace Checkpoint4._0
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Best Movies Ever!");
+            Console.WriteLine("Copyright, Adam Östberg 2018");
         }
     }
 }
